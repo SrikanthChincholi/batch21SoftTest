779fd65dee11db155146d3ea806a11064f6e82ee
srikanthchincholi <srikanthchincholi@yahoo.com>
2024-12-05 09:51:10 +0530

    test

---
+++ testSampleProj/src/looping/WhileClass2.java
@@ -0,0 +1,13 @@
+package looping;
+
+public class WhileClass2 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i <= 10) {
+			System.out.println("Value of i is : " + i);
+			i++;
+		}
+	}
+
+}
