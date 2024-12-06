dff5df4df64fc189109c077c2485d035039963f2
srikanthchincholi <srikanthchincholi@yahoo.com>
2024-12-06 09:42:30 +0530

    2 approaches of reading data

--- testSampleProj/src/filehandling/ReadDataFromExcel.java
+++ testSampleProj/src/filehandling/ReadDataFromExcel.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -15,6 +16,11 @@ public class ReadDataFromExcel {
 	static FileInputStream fis;
 
 	public static void main(String[] args) throws IOException {
+		readExcelData1();
+		readExcelData2();
+	}
+
+	public static void readExcelData1() throws IOException {
 		fis = new FileInputStream(filepath);
 		XSSFWorkbook wb = new XSSFWorkbook(fis);
 		XSSFSheet sh = wb.getSheet("TestData");
@@ -29,9 +35,32 @@ public class ReadDataFromExcel {
 				if (cell.getCellType() == CellType.STRING) {
 					System.out.print(cell.getStringCellValue() + " ");
 				} else if (cell.getCellType() == CellType.NUMERIC) {
-					System.out.print(cell.getNumericCellValue()+ " ");
+					System.out.print(cell.getNumericCellValue() + " ");
+				} else if (cell.getCellType() == CellType.BOOLEAN) {
+					System.out.print(cell.getBooleanCellValue() + " ");
 				}
 
+			}
+			System.out.println("");
+		}
+
+	}
+
+	public static void readExcelData2() throws IOException {
+		fis = new FileInputStream(filepath);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sh = wb.getSheet("TestData");
+
+		int nooforows = sh.getPhysicalNumberOfRows();
+		int noofcols = sh.getRow(0).getPhysicalNumberOfCells();
+
+		for (int i = 0; i < nooforows; i++) {
+			XSSFRow row = sh.getRow(i);
+			for (int j = 0; j < noofcols; j++) {
+				XSSFCell cell = row.getCell(j);
+				DataFormatter format = new DataFormatter();
+				String data = format.formatCellValue(cell);
+				System.out.print(data + " ");
 			}
 			System.out.println("");
 		}
