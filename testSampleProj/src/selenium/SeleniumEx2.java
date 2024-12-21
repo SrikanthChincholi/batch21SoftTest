6de9e9a5db595793bc698e98311a15f522a179fd
srikanthchincholi <srikanthchincholi@yahoo.com>
2024-12-21 10:16:51 +0530

    added code

--- testSampleProj/src/selenium/SeleniumEx2.java
+++ testSampleProj/src/selenium/SeleniumEx2.java
@@ -2,6 +2,7 @@ package selenium;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -11,42 +12,37 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumEx2 {
+	
+	static WebDriver d =null;
 
 	public static void main(String[] args) throws Exception {
 
-		WebDriver d = new ChromeDriver();
+		d = new ChromeDriver();
 		d.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		d.manage().window().maximize();
 		Thread.sleep(3000);
 		System.out.println(d.getTitle());
 		System.out.println(d.getCurrentUrl());
 		System.out.println(d.getPageSource());
-		File file = new File(".\\DataFiles\\PageSource.txt");
-		FileWriter fw = new FileWriter(file);
-		fw.write(d.getPageSource());
-		fw.close();
-		/*
-		 * String str[] = new String[1]; str[0] = "notepad.exe"; Process process =
-		 * Runtime.getRuntime().exec(str,null,file);
-		 */
-		WebElement imgs = d.findElement(By.xpath("//img[@alt]"));
+		getPageSource();
+		WebElement imgs = findElement(By.xpath("//img[@alt]"));
 		imgs.click();
 		// ((JavascriptExecutor)d).executeScript("arguments[0].style.border='3px solid
 		// red'", imgs);
 		Thread.sleep(3000);
-		List<WebElement> allimgs = d.findElements(By.xpath("//img[@alt]"));
+		List<WebElement> allimgs = findElements(By.xpath("//img[@alt]"));
 		if (allimgs.size() > 0) {
 			for (WebElement ele : allimgs) {
 				((JavascriptExecutor) d).executeScript("arguments[0].style.border='3px solid red'", ele);
-				}
+			}
 		} else {
 			System.out.println("No elements found ");
 		}
 
 		Thread.sleep(3000);
-		WebElement uname = d.findElement(By.name("username"));
+		WebElement uname = findElement(By.name("username"));
 		uname.sendKeys("admin");
-		WebElement pswd = d.findElement(By.name("password"));
+		WebElement pswd = findElement(By.name("password"));
 		pswd.sendKeys("admin123");
 		System.out.println(d.getWindowHandle());
 		WebElement submit = d.findElement(By.xpath("//button[@type='submit']"));
@@ -61,4 +57,22 @@ public class SeleniumEx2 {
 
 	}
 
+	public static void getPageSource() throws Exception {
+		File file = new File(".\\DataFiles\\PageSource.txt");
+		FileWriter fw = new FileWriter(file);
+		fw.write(d.getPageSource());
+		fw.close();
+
+	}
+	
+	public static WebElement findElement(By by)
+	{
+		return d.findElement(by);
+	}
+	
+
+	public static List<WebElement> findElements(By by)
+	{
+		return d.findElements(by);
+	}
 }
